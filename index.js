4050c5c57f2c1593a3d18b46e89b7c7fe06ef08a
Yankey Ange <nwolle14@gmail.com>
2025-11-18 03:25:59 +0000

    first commit

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('blog application')
+app.use(express.static('public'))
+app.use(express.static('images'))
+
+app.get("/", (req, res) => {
+  app.render("/")
 })
 
+app.set("views","./views")
+app.set("view engine", "ejs")
+
 app.listen(port, () => {
-  console.log(` l application demarre sur le port : ${port}`)
+  console.log('Serveur en écoute...')
 })
