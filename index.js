861ec21fd1daf0e86a830f54fbcafe8340010747
Yankey Ange <nwolle14@gmail.com>
2025-11-21 15:56:12 +0000

    styles.css

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const port = 3000
 app.use(express.static('public'))
 app.use(express.static('images'))
 
-app.get("/", (req, res) => {
-  app.render("/")
-})
+// EJS
+app.set('views', './views')
+app.set('view engine', 'ejs')
 
-app.set("views","./views")
-app.set("view engine", "ejs")
+//
+app.get('/', (req, res)=> {
+  res.render('header')
+})
 
 app.listen(port, () => {
   console.log('Serveur en écoute...')
